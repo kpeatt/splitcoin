3cd107a985bcfd81b79633f06d3c21fe641f68c5
Kyle Peatt <kpeatt@gmail.com>
2013-11-23 23:57:12 -0800

    Add comment about real data

--- app/js/services.js
+++ app/js/services.js
@@ -8,6 +8,7 @@ var splitcoinServices = angular.module('splitcoin.services', ['ngResource']);
 // In this case it is a simple value service.
 splitcoinServices.factory('Ticker', ['$resource',
     function($resource){
+        // For real data use: http://blockchain.info/ticker
         return $resource('/sampledata/ticker.json', {}, {
             query: {method:'GET'}
         });
