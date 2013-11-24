f842151151b53a8bf92e59def0411281f23db3bc
Kyle Peatt <kpeatt@gmail.com>
2013-11-23 23:35:26 -0800

    Fuck yah, it works.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,7 +5,36 @@ var splitcoin = angular.module('splitcoin.controllers', []);
 /* Controllers */
 
 splitcoin.controller('SplitCalc', function($scope, Ticker) {
+    function precise_round(num,decimals) {
+        return Math.round(num*Math.pow(10,decimals))/Math.pow(10,decimals);
+    }
+
     $scope.ticker = Ticker.query();
 
     $scope.selectedCurrency = "CAD";
+    $scope.result = '';
+    $scope.tipType = 'percent';
+    $scope.tip;
+    $scope.billTotal;
+
+    $scope.bitCalc = function(total, tip, tipType, currency) {
+        var btcTotal;
+
+        if (!tip) {
+            tip = 0;
+        }
+
+        total = Number(total);
+        tip = Number(tip);
+
+        if (tipType == 'percent') {
+            tip = total * (tip/100);
+        }
+
+        total = total + tip;
+
+        $scope.billTotal = total;
+        btcTotal = total / $scope.ticker[currency].last;
+        $scope.result = precise_round(btcTotal, 8);
+    }
 });
